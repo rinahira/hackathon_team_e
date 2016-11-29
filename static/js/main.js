eaa747b7a81d56ff90ecfcb99fa872e4f488ea77
Shinichi Shinoda <s-shinoda@sakura.ad.jp>
2016-11-29 13:45:59 +0900

    :tada: マウスオーバーでカートの中身を表示する

--- static/js/main.js
+++ static/js/main.js
@@ -3,4 +3,17 @@ $(function () {
         'resizeDuration': 200,
         'wrapAround': true
     });
+
+    var hoverTimeout;
+    $('.skay-cart').hover(
+        function () {
+            clearTimeout(hoverTimeout);
+            $('.skay-cart_table').show();
+        },
+        function () {
+            hoverTimeout = setTimeout(function () {
+                $('.skay-cart_table').hide();
+            }, 300);
+        }
+    )
 });
